841c5c79ad78b906aa278eb698e09027610751f9
DrParanoya <158059657+DrParanoya@users.noreply.github.com>
2024-11-24 20:07:55 +0100

    Add src\main.cpp
    Added the main cpp file. It contains the main function.
    It can read user input (file path) and open+read the contents of the file at the file path.

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
+
+int main()
+{
+    while (true) {
+        std::string input;
+        std::string path;
+        std::string bfcode;
+        std::uint16_t i = 0;
+        char mode = 'i';
+        bool random = false;
+        std::getline(std::cin, input);
+        while (true) {
+            if (input.length() == i) {
+                break;
+            }
+            else if ((input[i] == ' ' && input[i + 1] == '-')) {
+                if (input[i] != ' ' && input[i + 1] != '-') {
+                    if (input[i + 2] == '?') {
+                        random == true;
+                    }
+                    else if (input[i + 2] == 'i' || input[i + 2] == 'e' || input[i + 2] == 'c' || input[i + 2] == 'r' || input[i + 2] == 'n') {
+                        mode = input[i + 2];
+                    }
+                    else {
+                        std::cout << "Expected an identifier!\n";
+                    }
+                }
+                break;
+            }
+            path = path + static_cast<char>(input[i]);
+            ++i;
+            
+        }
+        
+        
+
+        std::cout << path << "\n";
+        
+        ++i;
+        //if (input[i+1]=='-' && input[i + 2] == 2){}
+
+        std::ifstream file(path);
+     
+        if (file.fail()) {
+            std::cout << "ERROR: failed to open the file at the given path \"" << path << "\"!\n";
+        }
+        else {
+            while (getline(file, bfcode)) {}
+        }
+    }
+}
