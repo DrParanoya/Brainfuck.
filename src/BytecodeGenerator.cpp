dc13bb0fae99e0ff3062a67c3104670308eba3f5
DrParanoya <158059657+DrParanoya@users.noreply.github.com>
2024-12-17 10:54:45 +0100

    good and bad solution at the same time

--- src/BytecodeGenerator.cpp
+++ src/BytecodeGenerator.cpp
@@ -1,11 +1,100 @@
 #include <iostream>
+#include <fstream>;
+#include <string>
 #include "BFHeader.h"
 
-bool byteGenerator(std::string bfcode, std::string path, bool random) {
-	for (std::uint32_t i = 0; i <= bfcode.length(); i++) {
-		if (!random) {
-			bfcode[i] = bfcode[i] == '?' ? ' ' : bfcode[i];
+bool byteGenerator(const std::string bfcode, std::string path, const bool random) {
+	std::ofstream file(path);
+	std::uint64_t change = 0;
+	std::uint64_t move = 0;
+	if (random) {
+		for (std::uint64_t i = 0; i <= bfcode.length(); i++) {
+			switch (bfcode[i]) {
+			case '?':
+				while (bfcode[i + 1] == '?') {
+					++i;
+				}
+				file << "rng";
+				break;
+			case '[':
+				file << "slp";
+				break;
+			case ']':
+				file << "elp";
+				break;
+			case '+':
+			case '-':
+				while (bfcode[i + 1] == '+' || bfcode[i] == '-') {
+					++i;
+					change = bfcode[i] == '+' ? change + 1 : change - 1;
+				}
+				file << "chg" << (change < 0 ? "-" + std::to_string((change * -1) % 127) : "+" + std::to_string(change % 127));
+				change = 0;
+				break;
+			case '>':
+			case '<':
+				while (bfcode[i + 1] == '>' || bfcode[i] == '<') {
+					++i;
+					change = bfcode[i] == '>' ? change + 1 : change - 1;
+				}
+				file << "mov" << (change < 0 ? "-" + std::to_string(change * -1) : "+" + std::to_string(change));
+				change = 0;
+				break;
+			case '.':
+				file << "out";
+				break;
+			case ',':
+				file << "inp";
+				break;
+			}
+			file << "\n";
 		}
 	}
+	else {
+		for (std::uint32_t i = 0; i <= bfcode.length(); i++) {
+			for (std::uint64_t i = 0; i <= bfcode.length(); i++) {
+				switch (bfcode[i]) {
+				case '?':
+					while (bfcode[i + 1] == '?') {
+						++i;
+					}
+					file << "rng";
+					break;
+				case '[':
+					file << "slp";
+					break;
+				case ']':
+					file << "elp";
+					break;
+				case '+':
+				case '-':
+					while (bfcode[i + 1] == '+' || bfcode[i] == '-') {
+						++i;
+						change = bfcode[i] == '+' ? change + 1 : change - 1;
+					}
+					file << "chg" << (change < 0 ? "-" + std::to_string((change * -1) % 127) : "+" + std::to_string(change % 127));
+					change = 0;
+					break;
+				case '>':
+				case '<':
+					while (bfcode[i + 1] == '>' || bfcode[i] == '<') {
+						++i;
+						change = bfcode[i] == '>' ? change + 1 : change - 1;
+					}
+					file << "mov" << (change < 0 ? "-" + std::to_string(change * -1) : "+" + std::to_string(change));
+					change = 0;
+					break;
+				case '.':
+					file << "out";
+					break;
+				case ',':
+					file << "inp";
+					break;
+				}
+				file << "\n";
+			}
+		}
+	}
+	file.close();
 	return true;
 }
